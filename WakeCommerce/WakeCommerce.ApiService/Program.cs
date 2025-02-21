7493391368dccf2cb72beb35984608ba8781c99f
Douetts <r_santos07@outlook.com>
2025-02-21 17:48:10 -0300

    docs(scalar) : biblioteca que vai nos permitir documentar os endpoint no .Net 9.

--- WakeCommerce/WakeCommerce.ApiService/Program.cs
+++ WakeCommerce/WakeCommerce.ApiService/Program.cs
@@ -1,15 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Scalar.AspNetCore;
+using WakeCommerce.ApiService.Middleware;
 
-// Add service defaults & Aspire client integrations.
-builder.AddServiceDefaults();
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddProblemDetails();
 
+builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//Config Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
+
+// Add Cors
+app.UseCors("AllowSpecificOrigin");
+
+// Add Scalar
+app.MapScalarApiReference(opt =>
+{
+    opt.Title = "Scalar - WakeCommerce";
+    opt.Theme = ScalarTheme.Mars;
+    opt.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+
+});
+
 
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
@@ -19,27 +46,4 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-string[] summaries = ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-app.MapDefaultEndpoints();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
