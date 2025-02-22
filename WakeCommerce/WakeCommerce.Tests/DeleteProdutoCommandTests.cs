5b1def021b9cd441cf5528b0ab309a5bd00747a6
Douetts <r_santos07@outlook.com>
2025-02-22 17:36:22 -0300

    test(produto): adicionar testes unitários para gerencimento de produto

---
+++ WakeCommerce/WakeCommerce.Tests/DeleteProdutoCommandTests.cs
@@ -0,0 +1,50 @@
+﻿
+using WakeCommerce.Application.Commands;
+
+namespace WakeCommerce.Unit.Tests
+{
+    public class DeleteProdutoCommandTests
+    {
+        [Fact]
+        public void EhValido_DeveRetornarVerdadeiro_QuandoIdForMaiorQueZero()
+        {
+            // Arrange
+            var command = new DeleteProdutoCommand(1);
+
+            // Act
+            var resultado = command.EhValido();
+
+            // Assert
+            Assert.Empty(command.ValidationResult.Errors);
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void EhValido_DeveRetornarFalso_QuandoIdForZero()
+        {
+            // Arrange
+            var command = new DeleteProdutoCommand(0);
+
+            // Act
+            var resultado = command.EhValido();
+
+            // Assert
+            Assert.False(resultado);
+            Assert.Equal(command.ValidationResult.Errors.First().ErrorMessage, "O Id deve ser maior que 0");
+        }
+
+        [Fact]
+        public void EhValido_DeveRetornarFalso_QuandoIdForNegativo()
+        {
+            // Arrange
+            var command = new DeleteProdutoCommand(-5);
+
+            // Act
+            var resultado = command.EhValido();
+
+            // Assert
+            Assert.False(resultado);
+            Assert.Equal(command.ValidationResult.Errors.First().ErrorMessage, "O Id deve ser maior que 0");
+        }
+    }
+}
---
+++ WakeCommerce/WakeCommerce.Tests/UpdateProdutoCommandTests.cs
@@ -0,0 +1,66 @@
+﻿using WakeCommerce.Application.Commands;
+
+namespace WakeCommerce.Unit.Tests
+{
+    public class UpdateProdutoCommandTests
+    {
+        [Fact]
+
+        public void EhValido_DeveRetornarVerdadeiro_QuandoDadosForemValidos()
+        {
+            // Arrange
+            var command = new UpdateProdutoCommand
+            {
+                Nome = "Produto Teste",
+                Preco = 100m,
+                Estoque = 10
+            };
+
+            // Act
+            var resultado = command.EhValido();
+
+            // Assert
+            Assert.Empty(command.ValidationResult.Errors);
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void EhValido_DeveRetornarFalso_QuandoNomeEstiverVazio()
+        {
+            // Arrange
+            var command = new UpdateProdutoCommand
+            {
+                Nome = "",
+                Preco = 100m,
+                Estoque = 10
+            };
+
+            // Act
+            var resultado = command.EhValido();
+
+            // Assert
+            Assert.False(resultado);
+            Assert.Equal(command.ValidationResult.Errors.First().ErrorMessage, "O nome não pode ser vazio");
+        }
+
+        [Fact]
+        public void EhValido_DeveRetornarFalso_QuandoPrecoForMenorQueZero()
+        {
+            // Arrange
+            var command = new UpdateProdutoCommand
+            {
+                Nome = "Produto Teste",
+                Preco = -1m,
+                Estoque = 10
+            };
+
+            // Act
+            var resultado = command.EhValido();
+
+            // Assert
+            Assert.False(resultado);
+            Assert.Equal(command.ValidationResult.Errors.First().ErrorMessage, "O preço não pode ser menor que 0");
+
+        }
+    }
+}
