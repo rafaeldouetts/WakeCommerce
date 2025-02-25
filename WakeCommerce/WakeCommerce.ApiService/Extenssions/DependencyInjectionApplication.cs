6f37ddc2f65820c9a62d5ad268174d2274c08021
Douetts <r_santos07@outlook.com>
2025-02-25 15:58:00 -0300

    refactor(dependencyInjection): melhoria na leitura do código

---
+++ WakeCommerce/WakeCommerce.ApiService/Extenssions/DependencyInjectionApplication.cs
@@ -0,0 +1,144 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using StackExchange.Redis;
+using System.Reflection;
+using WakeCommerce.Application.CommandHandlers;
+using WakeCommerce.Application.Commands;
+using WakeCommerce.Application.Mappings;
+using WakeCommerce.Application.Queries.Response;
+using WakeCommerce.Application.Queries;
+using WakeCommerce.Application.QueryHandlers;
+using WakeCommerce.Core.Mediator;
+using WakeCommerce.Core.Messages.CommonMessages.Notifications;
+using WakeCommerce.Domain.Repositories;
+using WakeCommerce.Domain.Repository;
+using WakeCommerce.Infrastructure.Data;
+using WakeCommerce.Infrastructure.Repository;
+using Serilog;
+using Serilog.Events;
+
+namespace WakeCommerce.ApiService.Extenssions
+{
+    public static class DependencyInjectionApplication
+    {
+        public static void AddDependencies(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            AddSerilog(builder);
+
+            // Configuração de serviços
+            builder.Services.AddProblemDetails();
+            builder.Services.AddControllers();
+            builder.Services.AddOpenApi();
+
+            //builder.Services.AddSerilogLogging();
+            builder.Services.AddDatabase(builder.Configuration);
+            builder.Services.AddOpenTelemetry(builder.Configuration);
+            builder.Services.AddRedisCache(builder.Configuration);
+            builder.Services.AddDICors(builder.Configuration);
+
+            builder.Services.AddRepository(builder.Configuration);
+            builder.Services.AddHandler(builder.Configuration);
+
+            // Registro do AutoMapper
+            builder.Services.AddAutoMapper(typeof(ProdutoProfile).Assembly);
+
+            // Registro do MediatR
+            builder.Services.AddMediatR(options =>
+            {
+                options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+        }
+
+        private static void AddSerilog(WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((ctx, lc) => lc
+                        .WriteTo.Console(LogEventLevel.Debug)
+                        .WriteTo.File($"log-{DateTime.UtcNow.ToString("dd/MM/yyyy")}.txt",
+                            LogEventLevel.Warning,
+                            rollingInterval: RollingInterval.Day)
+                        );
+        }
+
+        private static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<WakeCommerceDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        private static void AddOpenTelemetry(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOpenTelemetry()
+                    .ConfigureResource(resource => resource.AddService("WakeCommerceAPI"))
+                    .WithMetrics(metrics =>
+                    {
+                        metrics.AddAspNetCoreInstrumentation(); // Monitorar requisições HTTP
+                                                 //metrics.AddRuntimeInstrumentation();    // Monitorar .NET Runtime (CPU, GC, Threads)
+                        metrics.AddConsoleExporter();           // Exportar métricas para o console
+                        metrics.AddPrometheusExporter(options =>
+                        {
+                            options.ScrapeEndpointPath = "/metrics"; // Definir endpoint
+                            options.ScrapeResponseCacheDurationMilliseconds = 5000; // Atualizar a cada 5s
+                        });
+                    })
+                    .WithTracing(tracing =>
+                    {
+                        tracing.AddAspNetCoreInstrumentation(); // Monitorar requisições HTTP
+                        tracing.AddConsoleExporter();           // Exportar traces para o console
+                    });
+
+            // Configuração de HealthChecks
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("DefaultConnection"))
+                .AddRedis(configuration.GetConnectionString("RedisConnection")!, "Redis");
+        }
+
+        private static void AddRedisCache(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConnectionMultiplexer>(sp =>
+            {
+                var connection = configuration.GetConnectionString("RedisConnection");
+                return ConnectionMultiplexer.Connect(connection);
+            });
+
+            services.AddStackExchangeRedisCache(options =>
+            {
+                var connection = services.BuildServiceProvider().GetRequiredService<IConnectionMultiplexer>();
+                options.ConnectionMultiplexerFactory = () => Task.FromResult(connection);
+            });
+        }
+
+        private static void AddDICors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+        }
+
+        private static void AddRepository(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IRedisRepository, RedisRepository>();
+        }
+        private static void AddHandler(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IRequestHandler<CreateProdutoCommand, ProdutoResponse?>, CreateProdutoCommandHandler>();
+            services.AddScoped<IRequestHandler<UpdateProdutoCommand, ProdutoResponse?>, UpdateProdutoCommandHandler>();
+            services.AddScoped<IRequestHandler<DeleteProdutoCommand, bool>, DeleteProdutoCommandHandler>();
+
+            services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
+            services.AddScoped<IMediatorHandler, MediatorHandler>();
+
+            services.AddScoped<IFindProdutoQueryHandler, FindProdutoQueryHandler>();
+        }
+
+    }
+}
--- WakeCommerce/WakeCommerce.ApiService/Program.cs
+++ WakeCommerce/WakeCommerce.ApiService/Program.cs
@@ -1,29 +1,12 @@
 using Scalar.AspNetCore;
-using Serilog;
-using Serilog.Events;
 using WakeCommerce.ApiService.Middleware;
 using WakeCommerce.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using OpenTelemetry.Metrics;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
-using System.Reflection;
-using MediatR;
-using WakeCommerce.Application.Commands;
-using WakeCommerce.Application.CommandHandlers;
-using WakeCommerce.Core.Mediator;
-using WakeCommerce.Core.Messages.CommonMessages.Notifications;
-using WakeCommerce.Domain.Repositories;
-using WakeCommerce.Infrastructure.Repository;
-using WakeCommerce.Application.Queries;
-using WakeCommerce.Application.QueryHandlers;
-using WakeCommerce.Application.Queries.Response;
-using WakeCommerce.Domain.Repository;
-using StackExchange.Redis;
-using WakeCommerce.Application.EventHandlers;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using HealthChecks.UI.Client;
-using WakeCommerce.Application.Mappings;
+using WakeCommerce.ApiService.Extenssions;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,103 +21,14 @@ builder.Configuration
     .AddJsonFile($"appsettings.{environment}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-// Add services to the container.
-builder.Services.AddProblemDetails();
 
-builder.Services.AddControllers();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-//Config Cors
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigin", builder =>
-    {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
-    });
-});
-
-//Config Serilog 
 builder.Host.UseSerilog((ctx, lc) => lc
     .WriteTo.Console(LogEventLevel.Debug)
     .WriteTo.File($"log-{DateTime.UtcNow.ToString("dd/MM/yyyy")}.txt",
         LogEventLevel.Warning,
         rollingInterval: RollingInterval.Day));
 
-// Configuração do banco de dados (SQL Server)
-builder.Services.AddDbContext<WakeCommerceDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
-// Configurar OpenTelemetry com logs, métricas e tracing
-builder.Services.AddOpenTelemetry()
-    .ConfigureResource(resource => resource.AddService("WakeCommerceAPI"))
-    .WithMetrics(metrics =>
-    {
-        metrics.AddAspNetCoreInstrumentation(); // Monitorar requisições HTTP
-        //metrics.AddRuntimeInstrumentation();    // Monitorar .NET Runtime (CPU, GC, Threads)
-        metrics.AddConsoleExporter();           // Exportar métricas para o console
-        metrics.AddPrometheusExporter(options =>
-        {
-            options.ScrapeEndpointPath = "/metrics"; // Definir endpoint
-            options.ScrapeResponseCacheDurationMilliseconds = 5000; // Atualizar a cada 5s
-        });
-
-        // Exportar métricas para o Prometheus
-    })
-    .WithTracing(tracing =>
-    {
-        tracing.AddAspNetCoreInstrumentation(); // Monitorar requisições HTTP
-        tracing.AddConsoleExporter();           // Exportar traces para o console
-    });
-
-// Configurar a conexão com o Redis
-builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
-{
-    var configuration = builder.Configuration.GetConnectionString("RedisConnection");
-    return ConnectionMultiplexer.Connect(configuration);
-});
-
-// Registra o Redis como implementação do IDistributedCache
-builder.Services.AddStackExchangeRedisCache(options =>
-{
-    var connection = builder.Services.BuildServiceProvider().GetRequiredService<IConnectionMultiplexer>();
-
-    options.ConnectionMultiplexerFactory = () => Task.FromResult(connection); // Usa o IConnectionMultiplexer registrado para o Redis
-});
-
-builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
-builder.Services.AddScoped<IRedisRepository, RedisRepository>();
-
-builder.Services.AddScoped<INotificationHandler<DomainNotification>, DomainNotificationHandler>();
-builder.Services.AddScoped<IMediatorHandler, MediatorHandler>();
-builder.Services.AddScoped<IRequestHandler<CreateProdutoCommand, ProdutoResponse?>, CreateProdutoCommandHandler>();
-builder.Services.AddScoped<IRequestHandler<UpdateProdutoCommand, ProdutoResponse?>, UpdateProdutoCommandHandler>();
-builder.Services.AddScoped<IRequestHandler<DeleteProdutoCommand, bool>, DeleteProdutoCommandHandler>();
-builder.Services.AddScoped<IFindProdutoQueryHandler, FindProdutoQueryHandler> ();
-
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(ProdutoCreateEventHandler).Assembly));
-
-
-// Configurar Mediator
-builder.Services.AddMediatR(options =>
-{
-    options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-
-});
-
-builder.Services
-        .AddHealthChecks()
-        .AddSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-        .AddRedis(builder.Configuration.GetConnectionString("RedisConnection")!, "Redis");
-
-
-// add automapper 
-builder.Services.AddAutoMapper(typeof(ProdutoProfile).Assembly);
+builder.Services.AddDependencies(builder);
 
 var app = builder.Build();
 
